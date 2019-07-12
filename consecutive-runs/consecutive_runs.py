42ffbc0e81e81398dbcbfd21cf8e3e44dd74806d
Roland Kluge <roland.kluge@gmail.com>
2019-07-12 12:58:13 -0400

    Greatly increasing the readability of the code Adding a new test case for the empty/ None scenario

--- consecutive-runs/consecutive_runs.py
+++ consecutive-runs/consecutive_runs.py
@@ -5,35 +5,13 @@ import unittest
 def find_consecutive_runs(input_array):
     result = []
     
-    idx = 0
-    direction = None
-    count = 1
-    for i in range(1, len(input_array)):
-        if count == 3:
-            result.append(idx)
-            direction = None
-            idx = i
-            count = 1
-            continue
+    for i in range(len(input_array)-2):
+        diff_1 = input_array[i] - input_array[i+1]
+        diff_2 = input_array[i+1] - input_array[i+2]
 
-        diff = input_array[i] - input_array[i-1]
-        if direction == None:
-            idx=i-1
-            if diff == 1 or diff == -1:
-                direction = diff
-                count += 1
-        elif diff == direction:
-            count += 1
-        elif diff != direction:
-            if count == 3:
-                result.append(idx)
-            direction = None
-            idx = i
-            count = 1
-    
-    if count == 3:
-        result.append(idx)
-    
+        if diff_1 == diff_2 and abs(diff_1) == 1:
+            result.append(i)
+            
     return result
 
 class TestThis(unittest.TestCase):
@@ -45,5 +23,10 @@ class TestThis(unittest.TestCase):
     def test_given(self):
         given = [1, 2, 3, 5, 10, 9, 8, 9, 10, 11, 7]
         self.assertEqual(find_consecutive_runs(given), [0, 4, 6, 7])
+
+    def test_none(self):
+        none = []
+        self.assertEqual(find_consecutive_runs(none), [])
+
 
 unittest.main()
